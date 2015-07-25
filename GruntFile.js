0a0b8dfd40f3f8b862ce223ec373dbf1f6e1525b
Mateus Freira <mateus.freira@gmail.com>
2015-07-25 11:10:40 -0300

    update libreries

---
+++ GruntFile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		docbase: {
+			def: {
+				options: {
+					generatePath: "dist/",
+					baseUrl : "/"
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: './'
+				}
+			}
+		},
+		'gh-pages': {
+			options: {
+				base: 'dist'
+			},
+			src: ['**']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-docbase');
+	grunt.loadNpmTasks('grunt-gh-pages');
+	//grunt.loadTasks('node_modules/grunt-docbase/tasks');
+	// Default task.
+	grunt.registerTask('default', ['connect', 'docbase']);
+};
