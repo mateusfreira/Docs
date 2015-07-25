638e5a64b97d7df28ad505b46edc2d92894db1f0
Mateus Freira <mateus.freira@gmail.com>
2015-07-25 11:48:00 -0300

    To test hook

--- GruntFile.js
+++ GruntFile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 			def: {
 				options: {
 					generatePath: "dist/",
-					baseUrl : "/"
+					baseUrl: "/"
 				}
 			}
 		},
@@ -18,16 +18,16 @@ module.exports = function(grunt) {
 		},
 		'gh-pages': {
 			options: {
-				base: 'dist'
-			},
-			user: {
-				name: 'demo-travis-gh-pages',
-				email: 'doc@appbase.io'
-			},
-			repo: 'https://' + process.env.GH_TOKEN + '@github.com/mateusfreira/Docs.git',
-			message: 'publish gh-pages (auto)',
-			silent: true,
-			src: ['**']
+				base: 'dist',
+				user: {
+					name: 'demo-travis-gh-pages',
+					email: 'doc@appbase.io'
+				},
+				repo: 'https://' + process.env.GH_TOKEN + '@github.com/mateusfreira/Docs.git',
+				message: 'publish gh-pages (auto)',
+				silent: true,
+				src: ['**']
+			}
 		}
 	});
 
@@ -37,5 +37,5 @@ module.exports = function(grunt) {
 	// Default task.
 	grunt.registerTask('default', ['connect', 'docbase']);
 	grunt.registerTask('publish', ['connect', 'docbase', 'gh-pages']);
-	
+
 };
